d6fbbc74c45ac8d5b5535debfc0e7794d919f548
tomeeeS <sajtitamas1@hotmail.com>
2019-07-15 10:12:41 +0200

    fix Bag

--- Bag.kt
+++ Bag.kt
@@ -1,11 +1,19 @@
-class Bag<T : Comparable<T>> {
+class Bag<T> {
+    
     private val valuesMap = HashMap<T, Int>()
 
-    val cardinalities = valuesMap.values
-    val keys = valuesMap.keys
-    val keyCount = valuesMap.size
-    val totalSize = valuesMap.values.sum()
-    val most = valuesMap.toSortedMap().map { Pair(it.key, it.value) }.first()
+    val cardinalities
+        get() = valuesMap.values
+    val keys
+        get() = valuesMap.keys
+    val keyCount
+        get() = valuesMap.size
+    val totalSize
+        get() = valuesMap.values.sum()
+    val most
+        get() = valuesMap.entries.sortedByDescending { it.value }
+                                    .map { Pair(it.key, it.value) }
+                                    .first()
 
     fun add(value: T, pieceCount: Int = 1) {
         valuesMap[value] =
@@ -23,3 +31,15 @@ class Bag<T : Comparable<T>> {
 
     operator fun get(value: T) = valuesMap[value]
 }
+
+// prints 2
+fun main() = println(
+    Bag<Byte>().apply {
+        add(3)
+        add(4)
+        add(1)
+        add(1)
+        add(2, 4)
+        add(1)
+    }.most.first
+)
